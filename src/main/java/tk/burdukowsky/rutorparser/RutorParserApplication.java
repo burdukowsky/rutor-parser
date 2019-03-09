65d2560bd5940d98d641744ee889ea44d13f8f6f
burdukowsky <burdukowskystas@gmail.com>
2019-03-09 18:40:48 +0700

    add CORS filter

--- src/main/java/tk/burdukowsky/rutorparser/RutorParserApplication.java
+++ src/main/java/tk/burdukowsky/rutorparser/RutorParserApplication.java
@@ -2,12 +2,28 @@ package tk.burdukowsky.rutorparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class RutorParserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RutorParserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RutorParserApplication.class, args);
+    }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+        config.addExposedHeader("Content-Type");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
