c1dd502b90738ee7fb2f2f9ca64de2669da1deaa
burdukowsky <burdukowskystas@gmail.com>
2019-03-09 15:34:19 +0700

    initial commit

---
+++ src/main/java/tk/burdukowsky/rutorparser/RutorParserApplication.java
@@ -0,0 +1,13 @@
+package tk.burdukowsky.rutorparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutorParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutorParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/burdukowsky/rutorparser/RutorParserApplicationTests.java
@@ -0,0 +1,16 @@
+package tk.burdukowsky.rutorparser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RutorParserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
